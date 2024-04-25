1373d9434d97d3141628eebabfb857c37680dadd
LarsKarlsen82 <lpuk82@gmail.com>
2024-04-25 12:30:51 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import VitePluginRadar from 'vite-plugin-radar';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePluginRadar({
+      analytics: {
+        id: import.meta.env.VITE_PUBLIC_GA_ID,
+      },
+    })
+  ],
   base: './',
-})
+});
